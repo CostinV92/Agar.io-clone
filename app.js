a2c18e6858e7dd5fcc7596ba20cd3ef5c7606be8
Victor Gabriel <costinv92@gmail.com>
2017-05-01 14:12:02 +0300

    Add client server communication through socket.io
    Signed-off-by: Victor Gabriel <costinv92@gmail.com>

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 
 var app = express();
 var http = require('http').createServer(app);
-var port = 9090;
+
+var connection = require('./lib/connection.js')(http);
 
 app.use(express.static('public'));
 app.set('views', './views');
@@ -12,6 +13,7 @@ app.get('/', function(req, res) {
     res.render('index', {});
 });
 
+var port = 9090;
 http.listen(port, function() {
     console.log('Listening on ', port);
 });
