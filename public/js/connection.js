bb932ef0a8e952d41a3dee399674ea008fdc6aa1
Victor Gabriel <costinv92@gmail.com>
2017-05-01 15:46:12 +0300

    Add support to "register" users
    Signed-off-by: Victor Gabriel <costinv92@gmail.com>

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,7 +1,19 @@
 $( document ).ready(function() {
     var socket = io();
 
-    socket.on('message', function(msg){
+    socket.on('message', function(msg) {
         $('#messages').append($('<li>').text(msg));
     });
+
+    socket.on('registered', function(msg) {
+        // TODO here the game instance will be received
+        $('#important_messages').append($('<li>').text(msg));
+    });
+
+    register = function () {
+        if ($('#nickname').val()) {
+            socket.emit('register', $('#nickname').val());
+            $('#nickname').val('');
+        }
+    }
 });
